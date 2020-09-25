f9a50e28f878e48be8088a20c3b147da852b68fa
sikth 394 <avivw08@gmail.com>
2020-09-25 16:12:07 +0300

    basic gun animation

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public GameObject EnvHitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Environment")
+        {
+            GameObject effect = Instantiate(EnvHitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 5f);
+            Destroy(gameObject);
+        }
+        else
+        {
+            print("hi");
+        }
+         
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public GameObject GunFlashPrefab;
+
+    public float bulletforce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("ShootMain")){
+
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        GameObject GunFlash = Instantiate(GunFlashPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletforce, ForceMode2D.Impulse) ;
+
+
+    }
+}
+
